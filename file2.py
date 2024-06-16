fd6ddb5a13e2316ccee6f110e944678013954ecf
oliverjang1 <joonseojang960122@gmail.com>
2024-06-16 17:58:56 +0900

    숙제 2번

---
+++ file2.py
@@ -0,0 +1,11 @@
+a = float(input('첫 번째 실수를 입력하세요>>>'))
+b = float(input('두 번째 실수를 입력하세요>>>'))
+print('두 실수의 합은 {}입니다.'.format(a + b))
+
+def get_area(month):
+    days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    return days_in_month[month - 1]
+def 
+print("1~12 사이의 월을 입력하세요>>> ")
+
+
