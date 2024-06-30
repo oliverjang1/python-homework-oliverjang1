6cd1607ad8dc4ee20e677a7e5807506c4e79ce80
oliverjang1 <joonseojang960122@gmail.com>
2024-06-30 14:32:34 +0900

    예제 Pg 199 ~ 201

--- file8.py
+++ file8.py
@@ -6,9 +6,31 @@ def vending_machine(money):
 vending_machine(3000)
 
 def get_average(marks):
-    sum()
+    sum = 0
+
+    for key in marks:
+        score = marks[key]
+        sum += score
+
+    count = len(marks)
+
+    average = sum/count
     return average
 
 marks = {'국어': 90, '영어': 80, '수학': 85}
 average = get_average(marks)
 print('평균은 {}점입니다.'.format(average))
+
+total = 0
+
+def gift(dic, who, money):
+    dic[who] = money
+    global total
+    total += money
+
+wedding = {}
+gift(wedding, '영희', 5)
+gift(wedding, '철수', 3)
+gift(wedding, '이모', 10)
+print('축의금 명단: {}'.format(wedding))
+print('전체 축의금: {}'.format(total))
